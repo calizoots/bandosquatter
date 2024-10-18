88d5f3b44f2665d94529607041d73b93d851883b
cali zoots <calizoots@gmail.com>
2024-10-18 09:46:44 +0100

    fixed sum small

--- web/src/components/smallbits/Queue.tsx
+++ web/src/components/smallbits/Queue.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from "preact/hooks";
-import { State } from "../../pages/bludclart";
-import Player from "../MusicPlayerThatPlaysMusic";
-import "../styles/Queue.scss";
+import { useEffect, useRef } from 'preact/hooks';
+import type State from '../../types/bludclart';
+import Player from '../MusicPlayerThatPlaysMusic';
+import '../styles/Queue.scss';
 
 interface QueueProps {
-    player: Player
-    state: State
+    player: Player;
+    state: State;
 }
 
 let Queue: React.FC<QueueProps> = (props: QueueProps) => {
@@ -37,50 +37,47 @@ let Queue: React.FC<QueueProps> = (props: QueueProps) => {
     let showQueue = !props.player.shuffle ? (
         // for normal queue
         props.player.queue.length > 0 ? (
-            props.player.queue.map((song) =>
-                <div style={{ display: "flex", flexDirection: "column" }} className="queue-inner-wrapper" ref={queueInnerRef}>
-                    <span className="queue-song-name">
-                        {song.file.split(`${props.state.userData?.username}/`)[1].replace(".mp3", "")}
+            props.player.queue.map(song => (
+                <div style={{ display: 'flex', flexDirection: 'column' }} className="queue-inner-wrapper" ref={queueInnerRef}>
+                    <span className="queue-song-name">{song.file.split(`${props.state.userData?.username}/`)[1].replace('.mp3', '')}</span>
+                    <span style={{ fontSize: '12px' }} className="queue-remove-song" onClick={() => props.player.removeFromQueue(song.id)}>
+                        remove?
                     </span>
-                    <span style={{ fontSize: "12px" }} className="queue-remove-song" onClick={() => props.player.removeFromQueue(song.id)}>remove?</span>
                 </div>
-            )
+            ))
         ) : (
-            <div className="queue-inner-wrapper" style={{ textAlign: "center" }}>
+            <div className="queue-inner-wrapper" style={{ textAlign: 'center' }}>
                 <span className="queue-message-nothing">🙉 aint nun to see here 🙉</span>
             </div>
         )
+    ) : // for shuffle queue
+    // ik this comment seem real fucking basic but small change and i smoke weed
+    props.player.shuffledting.length > 0 ? (
+        props.player.shuffledting.map(song => (
+            <div style={{ display: 'flex', flexDirection: 'column' }} className="queue-inner-wrapper" ref={queueInnerRef}>
+                <span className="queue-song-name">{song.file.split(`${props.state.userData?.username}/`)[1].replace('.mp3', '')}</span>
+                <span style={{ fontSize: '12px' }} className="queue-remove-song" onClick={() => props.player.removeFromQueue(song.id)}>
+                    remove?
+                </span>
+            </div>
+        ))
     ) : (
-        // for shuffle queue
-        // ik this comment seem real fucking basic but small change and i smoke weed
-        props.player.shuffledting.length > 0 ? (
-            props.player.shuffledting.map((song) =>
-                <div style={{ display: "flex", flexDirection: "column" }} className="queue-inner-wrapper" ref={queueInnerRef}>
-                    <span className="queue-song-name">
-                        {song.file.split(`${props.state.userData?.username}/`)[1].replace(".mp3", "")}
-                    </span>
-                    <span style={{ fontSize: "12px" }} className="queue-remove-song" onClick={() => props.player.removeFromQueue(song.id)}>remove?</span>
-                </div>
-            )
-        ) : (
-            <div className="queue-inner-wrapper" style={{ textAlign: "center" }}>
-                <span className="queue-message-nothing">🙉 aint nun to see here 🙉</span>
-            </div>
-        )
-    )
+        <div className="queue-inner-wrapper" style={{ textAlign: 'center' }}>
+            <span className="queue-message-nothing">🙉 aint nun to see here 🙉</span>
+        </div>
+    );
 
     return (
         <>
             {props.state.isQueueVisible ? (
                 <div className={`queue-wrapper`}>
                     {/* cuz thats neccersarry im so tired */}
-                    <div className={`queue-inner-wrapper ${props.state.graphicalMode ? "" : "margin-fix-other-ui"}`}>
-                        {showQueue}
-                    </div>
-                </div> 
+                    <div className={`queue-inner-wrapper ${props.state.graphicalMode ? '' : 'margin-fix-other-ui'}`}>{showQueue}</div>
+                </div>
             ) : null}
         </>
-    )
-}
+    );
+};
 
 export default Queue;
+
